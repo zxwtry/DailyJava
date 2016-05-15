39b3dc4ee890a526ef2e878f200c6269049664a2
zxwtry <zxwtry@qq.com>
2016-05-15 22:27:51 +0800

    增加

--- src/java1605/干货_JVM并发性Java8并发性基础.java
+++ src/java1605/干货_JVM并发性Java8并发性基础.java
@@ -4,8 +4,13 @@ package java1605;
  * 	URL:	http://www.ibm.com/developerworks/cn/java/j-jvmc2/index.html
  * 	Title:	JVM 并发性: Java 8 并发性基础 
  * 	01:	Java8的并发改进：java.util.concurrent增加新的类和并行流。
- * 		并行流的设计目的：配合lambda
- * 	02：	
+ * 		并行流的设计目的：配合 lambda
+ * 	02：	分为三部分：
+ * 			一，使用 CompletableFuture 类协调异步操作；
+ * 			二，如何使用并行流
+ * 			三，Java8 新功能的执行方法
+ * 	03：	CompletableFuture<T> 类实现 CompletionStage<T> 接口
+ * 		
  * 
  */
 public class 干货_JVM并发性Java8并发性基础 {
