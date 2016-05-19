a3e723228e22c2dcc4daf544fa47ba838ac74b96
zxwtry <zxwtry@qq.com>
2016-05-19 21:04:57 +0800

    Java并发

--- src/java1605/干货_JVM并发性Java8并发性基础.java
+++ src/java1605/干货_JVM并发性Java8并发性基础.java
@@ -1,4 +1,10 @@
 package java1605;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
 /*
  * 	Source:	IBM developerworks
  * 	URL:	http://www.ibm.com/developerworks/cn/java/j-jvmc2/index.html
@@ -9,10 +15,110 @@ package java1605;
  * 			一，使用 CompletableFuture 类协调异步操作；
  * 			二，如何使用并行流
  * 			三，Java8 新功能的执行方法
- * 	03：	CompletableFuture<T> 类实现 CompletionStage<T> 接口
+ * 	03：	CompletableFuture<T> 类实现 CompletionStage<T> 接口，并扩展了Future<T>类
+ * 		CompletionStage　代表异步计算中的一个阶段或步骤
+ * 		CompletionStage  总共有59种方法, 比Future的５中要多。
+ * 	04:	Future 的用法:
+ * 		在Java8之前是只有两种：检查future是否完成和等待future完成
+ * 		Scala版本灵活得多，可以在future完成时执行回调函数，并以Throwable方式完成异常处理。
+ * 	05:	看ChunkDistanceChecker类
  * 		
  * 
  */
 public class 干货_JVM并发性Java8并发性基础 {
 
+}
+
+class ChunkDistanceChecker {
+    private final String[] knownWords;
+    public ChunkDistanceChecker(String[] knowns) {
+        knownWords = knowns;
+    }
+    /**
+    * Build list of checkers spanning word list.
+    * 
+    * @param words
+    * @param block
+    * @return checkers
+    */
+    public static List<ChunkDistanceChecker> buildCheckers(String[] words, int block) {
+        List<ChunkDistanceChecker> checkers = new ArrayList<>();
+        for (int base = 0; base < words.length; base += block) {
+            int length = Math.min(block, words.length - base);
+            checkers.add(new ChunkDistanceChecker(Arrays.copyOfRange(words, base, base + length)));
+        }
+        return checkers;    
+    }
+    /**
+     * Find best distance from target to any known word.
+     * 
+     * @param target
+     * @return best
+     */
+    public DistancePair bestDistance(String target) {
+        int[] v0 = new int[target.length() + 1];
+        int[] v1 = new int[target.length() + 1];
+        int bestIndex = -1;
+        int bestDistance = Integer.MAX_VALUE;
+        boolean single = false;
+        for (int i = 0; i < knownWords.length; i++) {
+            int distance = editDistance(target, knownWords[i], v0, v1);
+            if (bestDistance > distance) {
+                bestDistance = distance;
+                bestIndex = i;
+                single = true;
+            } else if (bestDistance == distance) {
+                single = false;
+            }
+        }
+        return single ? new DistancePair(bestDistance, knownWords[bestIndex]) :
+            new DistancePair(bestDistance);
+    }
+    private int editDistance(String target, String string, int[] v0, int[] v1) {
+		return 0;
+	}
+}
+class DistancePair {
+	public DistancePair(int bestDistance, String string) {
+	}
+	public DistancePair(int bestDistance) {
+	}
+	public static DistancePair worstMatch() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public static DistancePair best(DistancePair best, DistancePair join) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
+class CompletableFutureDistance0 extends TimingTestBase {
+    private final List<ChunkDistanceChecker> chunkCheckers;
+
+    private final int blockSize;
+
+    public CompletableFutureDistance0(String[] words, int block) {
+        blockSize = block;
+        chunkCheckers = ChunkDistanceChecker.buildCheckers(words, block);
+    }
+    public DistancePair bestMatch(String target) {
+    	if (blockSize == 0) {
+    		
+    	}
+        List<CompletableFuture<DistancePair>> futures = new ArrayList<>();
+        for (ChunkDistanceChecker checker: chunkCheckers) {
+            CompletableFuture<DistancePair> future =
+                CompletableFuture.supplyAsync(() -> checker.bestDistance(target));
+            futures.add(future);
+        }
+        DistancePair best = DistancePair.worstMatch();
+        for (CompletableFuture<DistancePair> future: futures) {
+            best = DistancePair.best(best, future.join());
+        }
+        return best;
+    }
+}
+
+class TimingTestBase {
+	
 }
---
+++ src/java1605/干货_JavaAPI_Future接口.java
@@ -0,0 +1,61 @@
+package java1605;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/*
+ * 	01	java.util.concurrent ：Future<V>  接口
+ * 		V 是Future的get方法得到的返回值
+ * 		所有已知子接口: 	Response<T>   RunnableFuture<V>   
+ * 						RunnableScheduledFuture<V>   ScheduledFuture<V>
+ * 		所有已知实现类:	FutureTask   SwingWorker
+ * 	02	Future 表示异步计算的接口。
+ * 			1,	Future提供了检查运行是否结束的方法，并等待运行结束，获得运行结果。
+ * 			2,	运行完成后，只能通过get方法来获得运行结果；可以配置运行结束前阻塞get方法。
+ * 			3,	通过cancel方法可以取消运行。
+ * 			4,	只为取消运行而采用Future，可以采用Future<?>类型，null作为底层任务结果。
+ * 	03	
+ * 		
+ */
+public class 干货_JavaAPI_Future接口 {
+	public static void main(String[] args) {
+
+	}
+
+}
+
+interface ArchiveSearcher {
+	String search(String target);
+}
+
+class App {
+	ExecutorService executor = Executors.newFixedThreadPool(4);
+	ArchiveSearcher searcher = null;
+
+	void showSearch(final String target) throws InterruptedException {
+		Future<String> future = executor.submit(new Callable<String>() {
+			public String call() {
+				return searcher.search(target);
+			}
+		});
+		displayOtherThings(); // do other things while searching
+		try {
+			displayText(future.get()); // use future
+		} catch (ExecutionException ex) {
+			cleanup();
+			return;
+		}
+	}
+
+	private void cleanup() {
+	}
+
+	private void displayText(String string) {
+	}
+
+	private void displayOtherThings() {
+	}
+}
