ae3b3a4da07f963b79fb2524fc1ec1441ed718d5
zxwtry <zxwtry@qq.com>
2016-05-20 17:37:05 +0800

    Edit Future

--- src/java1605/干货_JavaAPI_Future接口.java
+++ src/java1605/干货_JavaAPI_Future接口.java
@@ -2,9 +2,13 @@ package java1605;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+
+import com.sun.jmx.snmp.tasks.ThreadService;
 
 /*
  * 	01	java.util.concurrent ：Future<V>  接口
@@ -17,14 +21,40 @@ import java.util.concurrent.Future;
  * 			2,	运行完成后，只能通过get方法来获得运行结果；可以配置运行结束前阻塞get方法。
  * 			3,	通过cancel方法可以取消运行。
  * 			4,	只为取消运行而采用Future，可以采用Future<?>类型，null作为底层任务结果。
- * 	03	
+ * 	03	FutureTask是Future的实现类；FutureTask实现了Runnable接口。
  * 		
  */
 public class 干货_JavaAPI_Future接口 {
 	public static void main(String[] args) {
-		
+		try {
+			test01FutureTask();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-
+	static void test01FutureTask() throws InterruptedException, ExecutionException {
+		FutureTask<Integer> futureTask = new FutureTask<Integer>(new Callable<Integer>() {
+			@Override
+			public Integer call() throws Exception {
+				Thread.sleep(300);
+				System.out.println("Running");
+				return 1;
+			}
+		});
+		ExecutorService threadPool1 = Executors.newFixedThreadPool(4);
+//		for (int index = 0; index < 16; index ++) {
+		Future<?> future1 = threadPool1.submit(futureTask);
+		Thread.sleep(3000);
+//		}
+		threadPool1.shutdown();
+	}
+	static void showMsg(Future<?> future, int index) {
+		System.out.println(index + " : isDone : " + future.isDone());
+	}
 }
 
 interface ArchiveSearcher {
